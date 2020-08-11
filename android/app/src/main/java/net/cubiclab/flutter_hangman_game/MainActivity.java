86cdb74f85f7d704830826abc89c048379bed509
Samuel Ezedi <samuelezedi@gmail.com>
2020-08-11 19:05:33 +0100

    initial commit

---
+++ android/app/src/main/java/net/cubiclab/flutter_hangman_game/MainActivity.java
@@ -0,0 +1,6 @@
+package net.cubiclab.flutter_hangman_game;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HangMan',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: 'homePage',
+      routes: {
+        'homePage': (context) => HomeScreen(),
+        'scorePage': (context) => ScoreScreen(),
+      },
+    );
+  }
+}
